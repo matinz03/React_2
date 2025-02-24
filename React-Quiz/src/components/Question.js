187001023ae22e7dd1fd96cfb382439f36e252a7
matinz03 <74014122+matinz03@users.noreply.github.com>
2025-02-24 23:03:01 +0100

    Add files via upload

---
+++ React-Quiz/src/components/Question.js
@@ -0,0 +1,15 @@
+import { useReducer } from "react";
+import Options from "./Options";
+
+export default function Question({ question, answer, dispatch }) {
+  return (
+    <div>
+      <h4>{question.question}</h4>
+      <Options
+        question={question}
+        dispatch={dispatch}
+        answer={answer}
+      ></Options>
+    </div>
+  );
+}
